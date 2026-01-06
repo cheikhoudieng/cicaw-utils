4a7322896e816c87dfe5e0929f03f7b9154c11fc
cheikhoudieng <cheikhoudieng032@gmail.com>
2026-01-06 12:27:35 +0000

    update log name

--- remote_analyzer.py
+++ remote_analyzer.py
@@ -29,7 +29,7 @@ PA_USER = os.getenv("PA_USER", "Cicaw")
 PA_PASSWORD = os.getenv("PA_PASSWORD", "") 
 
 REMOTE_LOGS = [
-    "/home/Cicaw/cicaw_project/persistent_logs/db_traffic_v12.log",
+    "/home/Cicaw/cicaw_project/persistent_logs/db_traffic_v14.log",
     "/home/Cicaw/cicaw_project/persistent_logs/cmd_traffic.log"
 ]
 
